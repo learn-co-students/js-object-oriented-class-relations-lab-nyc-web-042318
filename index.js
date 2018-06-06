22e2c32e264de1795195fa82cdbe4445ef268e4e
eanorton <edwardnorton@icloud.com>
2018-06-06 17:54:57 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,67 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let driverId = 0
+
+class Driver {
+  constructor(name) {
+    this.name = name
+    this.id = ++driverId
+
+    store.drivers.push(this)
+  }
+
+  trips(){
+    return store.trips.filter(trip => trip.driverId === this.id)
+  }
+
+  passengers(){
+    return this.trips().map(trip => trip.passenger())
+  }
+
+}
+
+let passengerId = 0
+
+class Passenger {
+  constructor(name){
+    this.name = name
+    this.id = ++passengerId
+
+    store.passengers.push(this)
+  }
+
+  trips(){
+    return store.trips.filter(trip => trip.passengerId === this.id)
+  }
+
+  drivers(){
+    return this.trips().map(trip=>trip.driver())
+  }
+
+}
+
+let tripId = 0
+
+class Trip {
+  constructor(driver, passenger){
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    this.id = ++tripId
+
+    store.trips.push(this)
+  }
+
+  driver(){
+    return store.drivers.find(function(driver){
+      return driver.id === this.driverId
+    }.bind(this))
+  }
+
+  passenger(){
+    return store.passengers.find(function(passenger){
+      return passenger.id === this.passengerId
+    }.bind(this))
+  }
+
+
+}
